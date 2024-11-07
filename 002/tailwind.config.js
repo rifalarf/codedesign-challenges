76aabd9754408361ad31cd7693b156cd61dbe5d6
rifalarf <rifalarifrahman23@gmail.com>
2024-11-07 17:23:13 +0700

    .

--- 002/tailwind.config.js
+++ 002/tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
